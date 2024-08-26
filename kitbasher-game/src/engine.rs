2755cc01225fcc43515b6c03d88a887f40effbf4
Ben Giacalone <bensgiacalone@gmail.com>
2024-08-25 22:09:35 -0400

    Add rotation logic

--- kitbasher-game/src/engine.rs
+++ kitbasher-game/src/engine.rs
@@ -2,17 +2,111 @@ use bevy::math::{Quat, Vec3};
 use serde::{Deserialize, Serialize};
 
 /// An instance of the kitbasher engine.
-pub struct KBEngine {}
+pub struct KBEngine {
+    parts: Vec<PartData>,
+    connect_rules: Vec<[usize; 2]>,
+    model: Vec<PlacedConfig>,
+}
 
 impl KBEngine {
     /// Creates a new instance of the engine.
-    pub fn new(_parts: &[PartData], _connect_rules: &[[usize; 2]]) -> Self {
-        Self {}
+    pub fn new(parts: &[PartData], connect_rules: &[[usize; 2]]) -> Self {
+        Self {
+            parts: parts.into(),
+            connect_rules: connect_rules.into(),
+            model: Vec::new(),
+        }
     }
 
     /// Returns the set of valid next placements.
     pub fn gen_candidates(&self) -> Vec<PlacedConfig> {
-        todo!()
+        let mut configs = Vec::new();
+        for (part_id, part) in self.parts.iter().enumerate() {
+            let z_rot = 0;
+            for x_rot in 0..4 {
+                for y_rot in 0..4 {
+                    // Rotate part
+                    let mut connectors = Vec::new();
+                    for connector in &part.connectors {
+                        let mut axis = connector.axis;
+                        let mut side_a = connector.side_a;
+                        let mut position = connector.position;
+                        for (axis_type, num_turns) in
+                            [(Axis::X, x_rot), (Axis::Z, z_rot), (Axis::Y, y_rot)]
+                        {
+                            let normal = match axis_type {
+                                Axis::X => Vec3::X,
+                                Axis::Y => Vec3::Y,
+                                Axis::Z => Vec3::Z,
+                            };
+                            let rot = Quat::from_axis_angle(normal, std::f32::consts::PI / 2.);
+                            let (axis1, axis2) = match axis_type {
+                                Axis::X => (Axis::Z, Axis::Y),
+                                Axis::Y => (Axis::X, Axis::Z),
+                                Axis::Z => (Axis::X, Axis::Y),
+                            };
+                            for _ in 0..num_turns {
+                                if axis == axis1 && connector.side_a {
+                                    side_a = false;
+                                } else if connector.axis == axis2 && !connector.side_a {
+                                    side_a = true;
+                                }
+                                // Swap axis to other plane axis
+                                axis = if axis == axis1 {
+                                    axis2
+                                } else if axis == axis2 {
+                                    axis1
+                                } else {
+                                    axis
+                                };
+                                position = rot.mul_vec3(position);
+                            }
+                        }
+                        let new_connector = Connector {
+                            side_a,
+                            axis,
+                            connect_type: connector.connect_type,
+                            position,
+                        };
+                        connectors.push(new_connector);
+                    }
+                    let mut bboxes = Vec::new();
+                    for bbox in &part.bboxes {
+                        let mut center = bbox.center;
+                        let mut half_sizes = bbox.half_sizes;
+                        for (axis_type, num_turns) in
+                            [(Axis::X, x_rot), (Axis::Z, z_rot), (Axis::Y, y_rot)]
+                        {
+                            let normal = match axis_type {
+                                Axis::X => Vec3::X,
+                                Axis::Y => Vec3::Y,
+                                Axis::Z => Vec3::Z,
+                            };
+                            let rot = Quat::from_axis_angle(normal, std::f32::consts::PI / 2.);
+                            for _ in 0..num_turns {
+                                center = rot.mul_vec3(center);
+                                half_sizes = match axis_type {
+                                    Axis::X => Vec3::new(half_sizes.x, half_sizes.z, half_sizes.y),
+                                    Axis::Y => Vec3::new(half_sizes.z, half_sizes.y, half_sizes.x),
+                                    Axis::Z => Vec3::new(half_sizes.y, half_sizes.x, half_sizes.z),
+                                }
+                            }
+                        }
+                        let new_bbox = AABB { center, half_sizes };
+                        bboxes.push(new_bbox);
+                    }
+                    let new_part = PartData {
+                        bboxes,
+                        model_path: part.model_path.clone(),
+                        connectors,
+                    };
+                }
+            }
+            for z_rot in [1, 3] {
+                for y_rot in 0..4 {}
+            }
+        }
+        configs
     }
 
     /// Places a part on the model.
@@ -21,12 +115,14 @@ impl KBEngine {
     pub fn place_part(&mut self, _placement: &PlacedConfig) {}
 
     /// Returns the current model.
-    pub fn get_model(&self) -> Vec<PlacedConfig> {
-        todo!()
+    pub fn get_model(&self) -> &[PlacedConfig] {
+        &self.model
     }
 
     /// Clears the current model.
-    pub fn clear_model(&mut self) {}
+    pub fn clear_model(&mut self) {
+        self.model = Vec::new();
+    }
 }
 
 /// AABB bounding box.
@@ -37,11 +133,11 @@ pub struct AABB {
 }
 
 /// Denotes an axis.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum Axis {
     X,
     Y,
-    Z
+    Z,
 }
 
 /// Information on how parts can snap to each other.
@@ -54,7 +150,6 @@ pub struct Connector {
     pub connect_type: usize,
     pub position: Vec3,
 }
-
 
 /// Basic data for a part.
 #[derive(Debug, Clone, Serialize, Deserialize)]
